dbf84b2869194136de899fa7687e7c5d6862e40e
Nitish Kumar Verma <nitishkumarverma25@gmail.com>
2023-01-04 22:38:50 +0530

    dependency added and Static variable file created

---
+++ Flipkart/src/main/java/org/example/constants/Constants.java
@@ -0,0 +1,18 @@
+package org.example.constants;
+
+import org.openqa.selenium.WebDriver;
+
+public class Constants {
+    public static String DRIVER_PATH = "C://code//driver1//msedgedriver.exe";
+    public static String EDGE_DRIVER = "webdriver.edge.driver";
+    public static String GOOGLE_URL = "https://www.google.com";
+
+    public static String ASSIGNMENT6_URL = "https://www.rahulshettyacademy.com/AutomationPractice/";
+    public static int ASSIGNMENT6_OPTION = 1;
+
+    final String username = "6290397034";
+    final String password = "Nitish@123";
+    public WebDriver driver;
+    final String url = "https://www.flipkart.com/";
+
+}
